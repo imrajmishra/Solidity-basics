e58de9c5d7f0ca90c6963f7de797f6e1a817ff97
Raj Mishra <raj212558@gmail.com>
2026-06-20 08:12:25 +0530

    feat: add SimpleStorage contract with store and retrieve functions

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,6 +1,16 @@
-// SPDX-Licence-Identifier: UNLINENCED
-pragma solidity 0.8.35;  // This is the solidity version
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.35;
 
 contract SimpleStorage {
-    // Technically This is a smart contract.
+
+    uint256 public favoriteNumber;
+
+    function store(uint256 _number) public {
+        favoriteNumber = _number;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
 }
