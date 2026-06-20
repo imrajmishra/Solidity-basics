c94472de16c87432148b579fad0518ef73f9cc03
Raj Mishra <raj212558@gmail.com>
2026-06-20 08:08:55 +0530

    feat: add all Solidity data types to SimpleStorage contract

---
+++ data-types/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-Licence-Identifier: UNLINENCED
+
+pragma solidity 0.8.35;
+
+contract SimpleStorage {
+    // Basic Types: uint, int, boolean, address, bytes
+
+    bool hasFavorateNumber = true;
+    uint256 currenAge = 22;
+    int256 currentBalance = 20000;
+    address myAddress = 0x7eD04EF0c2dc4952d63088CE60f514089fF3383b;
+    string myName = "Raj Mishra";
+    bytes32 hashValue = "thisIsRajMishra";
+}
