6bbeeaddfc5ed8f2db19b21427400c510038bcf2
Raj Mishra <raj212558@gmail.com>
2026-06-22 20:27:23 +0530

    feat: add SimpleStorage1 contract with dynamic and static arrays for storing persons

---
+++ functions/Arrays.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.35;
+
+contract SimpleStorage1 {
+
+
+    uint myfavoriteNumber; // 0
+
+    // uint256[] public listOfFavoriteNumber;  
+
+    struct Person {
+        uint favoriteNumber;
+        string name;
+    }
+
+    // Dynamic array
+    Person[] public listOfPersons;
+
+    // Static array
+    Person[4] public listOfFavoritePersons;
+
+    
+
+
+   function addPersons(string memory _name, uint256 _favoriteNumber) public {
+        // Person memory newPerson = Person(_favoriteNumber, _name);
+        // listOfPersons.push(newPerson);
+
+        listOfPersons.push(Person(_favoriteNumber, _name));
+   }
+
+}
